25145adc7165e785dfe883734015aa41099d3bea
Damileee <olasojudamilare6@gmail.com>
2023-10-25 16:47:35 +0100

    chore: created the sign in form

--- src/components/login/SignInForm.js
+++ src/components/login/SignInForm.js
@@ -1,27 +1,61 @@
+import React, { useState } from "react";
+import Button from "../forms/Button";
 import Input from "../forms/Input";
 
 const SignInForm = () => {
+  const [isButtonProcessing, setIsButtonProcessing] = useState(false);
+  const isButtonDisabled = isButtonProcessing;
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    console.log(e);
+
+    // Handle your form submission logic here
+    setIsButtonProcessing(true);
+
+    // Simulate some asynchronous operation, e.g., making an API request
+    // Replace this with your actual asynchronous operation
+    // For this example, I'll simulate a 2-second operation
+    setTimeout(() => {
+      // After the operation is done, set isButtonProcessing to false
+      setIsButtonProcessing(false);
+    }, 2000); // Simulate a 2-second operation
+  };
+
   return (
     <div>
-      <h1>Sign In</h1>
-      <p>Welcome Back, please login below</p>
-      <div>
-        <div>
-          <Input 
-          label="Email Address" 
-          type="email" 
-          className="input-field" 
-          placeholder=""
-          />
-        </div>
-        <div>
+      <div className="sign-in-form-header">
+        <h1>Sign In</h1>
+        <span>Welcome Back, please login below</span>
+      </div>
+      <div className="sign-in-form">
+        <form onSubmit={handleFormSubmit}>
+          <div>
             <Input
-            label="Password"
-            type="password"
-            className="input-field"
-            placeholder=""
+              label="Email Address"
+              type="email"
+              className="input-field"
+              placeholder=""
             />
-        </div>
+          </div>
+          <div>
+            <Input
+              label="Password"
+              type="password"
+              className="input-field"
+              placeholder=""
+            />
+          </div>
+          <div>
+            <Button
+              label="SIGN IN"
+              type="submit" // i'm Using type="submit" to trigger form submission
+              isDisabled={isButtonDisabled}
+              isProcessing={isButtonProcessing}
+              className="custom-button"
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
