98b5dc23f42ac84fb627d17613e0611d1d686f7e
Damileee <olasojudamilare6@gmail.com>
2023-11-02 16:49:37 +0100

    Chore: implemented route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
-import Form from "./router";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignInForm from "./components/login/SignInForm";
+import SignUpForm from "./components/login/SignUpForm";
+import About from "./components/pages/About";
+import Membership from "./components/pages/Membership";
+import Home from "./components/pages/Home";
+import TransitionWrapper from "./components/login/TransitionWrapper";
 
 function App() {
   return (
-    <div className="App">
-      <div className="content">
-        <Navbar />
-        <div>
-          <Form />
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Navbar />
+          <div>
+            <Route
+              render={({ location }) => (
+                <TransitionWrapper location={location}>
+                  <Switch location={location}>
+                    <Route path="/login" component={SignInForm} />
+                    <Route path="/create-account" component={SignUpForm} />
+                  </Switch>
+                </TransitionWrapper>
+              )}
+            />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-page" component={About} />
+              <Route path="/membership" component={Membership} />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
