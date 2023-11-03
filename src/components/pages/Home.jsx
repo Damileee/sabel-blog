a13b42c55b6395f458f90d4baa9401d912bd87e5
Damileee <olasojudamilare6@gmail.com>
2023-11-03 16:16:47 +0100

    Chore: working on the home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import DefaultLayout from "../../layout/Default";
+import LadyBagImage from "../../assets/images/home-page_first-img.png"
 
 const Home = () => {
     return (
         <DefaultLayout>
             <div>
                 <h1>This month’s finest</h1>
+                <img src={LadyBagImage} alt="" />
             </div>
         </DefaultLayout>
     );
