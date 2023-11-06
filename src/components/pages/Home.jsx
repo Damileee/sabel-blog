7cf15dbff86ec4e7d6f3707c27be32a80aaf79d7
Damileee <olasojudamilare6@gmail.com>
2023-11-06 16:24:01 +0100

    Chore: working on the home page ui

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,18 @@
 import DefaultLayout from "../../layout/Default";
-import FirstImg from "../../assets/images/homefirstimg.png";
+import FirstImg from "../../assets/images/home-page_first-img.png";
 import FlexImgOne from "../../assets/images/fleximg1.png";
 import FlexImgTwo from "../../assets/images/fleximg2.png";
 import FlexImgThree from "../../assets/images/fleximg3.png";
+import SecondImg from "../../assets/images/home-page_second-img.png";
+import Button from "../forms/Button";
 
 const Home = () => {
   return (
     <DefaultLayout>
-      <div>
+      <div className="home_page-container">
         <h1>This month’s finest</h1>
         <div>
-          <img src={FirstImg} alt="" />
+          <img src={FirstImg} alt="" className="home_page_img-container" />
         </div>
         <h2>Title goes here</h2>
         <span>Photography by Peter Jordanov</span>
@@ -27,6 +29,19 @@ const Home = () => {
           <img src={FlexImgOne} alt="" />
           <img src={FlexImgTwo} alt="" />
           <img src={FlexImgThree} alt="" />
+        </div>
+        <div>
+          <img src={SecondImg} alt="" />
+        </div>
+        <div>
+          <span>Take my blues away</span>
+          <span>Photography by Peter Jordanov Interview by Isabelle Zeuch</span>
+          <span>Lebanese renaissance woman HALA MOAWAD creates one-off leather garments from fleamarket pieces and appliquéd off-cuts. She gives these pieces a new life, rising out of the remnants like a phoenix from the flames.</span>
+        </div>
+        <div>
+          <Button
+          label="READ MORE" 
+          />
         </div>
       </div>
     </DefaultLayout>
