b0dd409a702c67ba020dead617511c06d6d8fa62
Damileee <olasojudamilare6@gmail.com>
2023-11-07 15:36:06 +0100

    Chore: working on the home page ui

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,41 +11,47 @@ const Home = () => {
     <DefaultLayout>
       <div className="home_page-container">
         <h1>This month’s finest</h1>
-        <div>
-          <img src={FirstImg} alt="" className="home_page_img-container" />
+        <div className="home_page_img-container">
+          <img src={FirstImg} alt=""/>
         </div>
         <div>
-        <h2>Title goes here</h2>
-        <div className="home_page-first-span">
-        <span>Photography by Peter Jordanov</span>
+          <h2>Title goes here</h2>
+          <div className="home_page-first-span">
+            <span>Photography by Peter Jordanov</span>
+          </div>
+          <div className="home_page-second-span">
+            <span>
+              Sustainable fashion is not just a trend, it is a movement that is
+              changing the way we think about fashion. It is a movement that is
+              empowering designers, brands, and consumers to make conscious
+              choices and to create a better future for ourselves and for
+              generations to come. Let us join together in this movement and
+              imagine a world where fashion is not only beautiful, but also
+              sustainable and ethical ...
+              <span className="fashion">Read More</span>
+            </span>
+          </div>
         </div>
-        <span>
-          Sustainable fashion is not just a trend, it is a movement that is
-          changing the way we think about fashion. It is a movement that is
-          empowering designers, brands, and consumers to make conscious choices
-          and to create a better future for ourselves and for generations to
-          come. Let us join together in this movement and imagine a world where
-          fashion is not only beautiful, but also sustainable and ethical ...
-          Read More
-        </span>
-        </div>
-        <div>
+        <div className="home_page-img-container">
           <img src={FlexImgOne} alt="" />
           <img src={FlexImgTwo} alt="" />
           <img src={FlexImgThree} alt="" />
         </div>
-        <div>
-          <img src={SecondImg} alt="" className="home_page_img-container" />
+        <div className="home_page_img-container">
+          <img src={SecondImg} alt=""/>
         </div>
         <div>
           <span>Take my blues away</span>
           <span>Photography by Peter Jordanov Interview by Isabelle Zeuch</span>
-          <span>Lebanese renaissance woman HALA MOAWAD creates one-off leather garments from fleamarket pieces and appliquéd off-cuts. She gives these pieces a new life, rising out of the remnants like a phoenix from the flames.</span>
+          <span>
+            Lebanese renaissance woman HALA MOAWAD creates one-off leather
+            garments from fleamarket pieces and appliquéd off-cuts. She gives
+            these pieces a new life, rising out of the remnants like a phoenix
+            from the flames.
+          </span>
         </div>
         <div>
-          <Button
-          label="READ MORE" 
-          />
+          <Button label="READ MORE" />
         </div>
       </div>
     </DefaultLayout>
