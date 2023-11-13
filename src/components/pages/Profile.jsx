aac938a40fa8bd7b5663c09b10e5ad43ab319887
Damileee <olasojudamilare6@gmail.com>
2023-11-13 16:42:39 +0100

    Chore: created the profile page component

---
+++ src/components/pages/Profile.jsx
@@ -0,0 +1,17 @@
+const Profile = () => {
+    return ( 
+        <div>
+            <header>
+                <h1>Profile</h1>
+            </header>
+            <div>
+                <span>Profile Settings</span>
+                <div></div>
+                <span>Profile Picture</span>
+                <span>You can upload a JPG or PNG file. Maximum file size is 2MB.</span>
+            </div>
+        </div>
+    );
+}
+ 
+export default Profile;
